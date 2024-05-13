1ed3fb5866a9681c6fedcaaa0d245e3f4735aaf2
haonguyen7920 <135431300+haonguyen7920@users.noreply.github.com>
2024-05-13 16:00:05 -0700

    Update spaceInvader.js

--- spaceInvader.js
+++ spaceInvader.js
@@ -354,8 +354,8 @@ function spaceInvader (window, canvas) {
     };
   document.getElementById("myBtn").addEventListener("click", function () {
     new Game();
-    // var canvasElm = document.getElementById("space-invader");
-    // canvasElm.setAttribute("tabindex", "0");
-    // canvasElm.focus();
+    var canvasElm = document.getElementById("space-invader");
+    canvasElm.setAttribute("tabindex", "0");
+    canvasElm.focus();
   });
 }
